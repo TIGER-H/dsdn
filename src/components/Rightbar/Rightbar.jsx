e3776cb943e50fe9af74d23c6a83b555d37f70ce
yeoreum <549747168@qq.com>
2021-12-30 20:06:41 +0800

    right bar rankList

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -25,51 +25,52 @@ const Rightbar = () => {
           <Avatar size={80} name="top2" variant="beam" />
           <div className="rightbarTop3Badge">{silverMedal}</div>
           <div className="rightbarTopThreeItemName">
-            {rankList[1]?.nickName || "top2"}
+            {(rankList && rankList[1]?.nickName) || "top2"}
           </div>
           <div className="rightbarTopThreeItemValue">
             {<MdFavoriteBorder />}
-            {rankList[1]?.num || 0}
+            {(rankList && rankList[1]?.num) || 0}
           </div>
         </div>
         <div className="rightbarTopThreeItem">
           <Avatar size={80} name="top1" variant="beam" />
           <div className="rightbarTop3Badge">{goldMedal}</div>
           <div className="rightbarTopThreeItemName">
-            {rankList[0]?.nickName || "top1"}
+            {(rankList && rankList[0]?.nickName) || "top1"}
           </div>
           <div className="rightbarTopThreeItemValue">
             {<MdFavoriteBorder />}
-            {rankList[0]?.num || 0}
+            {(rankList && rankList[0]?.num) || 0}
           </div>
         </div>
         <div className="rightbarTopThreeItem">
           <div className="rightbarTop3Badge">{bronzeMedal}</div>
           <Avatar size={80} name="top3" variant="beam" />
           <div className="rightbarTopThreeItemName">
-            {rankList[2]?.nickName || "TOP3"}
+            {(rankList && rankList[2]?.nickName) || "TOP3"}
           </div>
           <div className="rightbarTopThreeItemValue">
             {<MdFavoriteBorder />}
-            {rankList[2]?.num || 0}
+            {(rankList && rankList[2]?.num) || 0}
           </div>
         </div>
       </div>
       <ul className="rightbarTopElse">
-        {rankList.slice(3).map((item, index) => (
-          <li className="rightbarTopElseItem" key={index}>
-            <span className="rightbarTopElseRank">{index + 4}</span>
-            {item?.imageUrl ? (
-              <img src={item.imageUrl} alt="not found" />
-            ) : (
-              <Avatar size={54} name={`top${index + 4}`} variant="beam" />
-            )}
-            <span className="rightbarTopElseName">
-              {item?.nickName || `top ${index + 4}`}
-            </span>
-            <span className="rightbarTopElseValue">{item?.num || 0}</span>
-          </li>
-        ))}
+        {rankList &&
+          rankList.slice(3).map((item, index) => (
+            <li className="rightbarTopElseItem" key={index}>
+              <span className="rightbarTopElseRank">{index + 4}</span>
+              {item?.imageUrl ? (
+                <img src={item.imageUrl} alt="not found" />
+              ) : (
+                <Avatar size={54} name={`top${index + 4}`} variant="beam" />
+              )}
+              <span className="rightbarTopElseName">
+                {item?.nickName || `top ${index + 4}`}
+              </span>
+              <span className="rightbarTopElseValue">{item?.num || 0}</span>
+            </li>
+          ))}
       </ul>
     </div>
   );
